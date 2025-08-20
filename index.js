d38d7c4ea0fe01ff2a140a924e221e0db33ec471
stanleyfluke <82741305+stanleyfluke@users.noreply.github.com>
2025-08-20 15:47:47 +0800

    commit all

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function checkPin1(){
     const input = document.getElementById("pin1").value;
     const present1 = document.getElementById("present1");
+    const text1 = document.getElementById("text1");
 
     const correctPin = "5135";
 
     if (input === correctPin) {
         present1.style.display = "block";
+        text1.style.display = "block";
       } else {
         alert("Wrong PIN! Try again.");
       }
@@ -14,11 +16,13 @@ function checkPin1(){
 function checkPin2(){
     const input = document.getElementById("pin2").value;
     const present2 = document.getElementById("present2");
+    const text2 = document.getElementById("text2");
 
     const correctPin = "7241";
 
     if (input === correctPin) {
         present2.style.display = "block";
+        text2.style.display = "block";
       } else {
         alert("Wrong PIN! Try again.");
       }
@@ -26,12 +30,14 @@ function checkPin2(){
 
 function checkPin3(){
     const input = document.getElementById("pin3").value;
-    const present2 = document.getElementById("present3");
+    const present3 = document.getElementById("present3");
+    const text3 = document.getElementById("text3");
 
     const correctPin = "7146";
 
     if (input === correctPin) {
-        present2.style.display = "block";
+        present3.style.display = "block";
+        text3.style.display = "block";
       } else {
         alert("Wrong PIN! Try again.");
       }
